697b9d549c64c9308c8b6eabb3a6bbf321fdc52b
Mostaq27 <mostaq99muzahid@gmail.com>
2023-11-06 16:18:43 +0600

    added sociallogin components

--- src/Components/Shared/SocialLogin/SocialLogin.jsx
+++ src/Components/Shared/SocialLogin/SocialLogin.jsx
@@ -1,1 +1,25 @@
+
+// import { useContext } from "react";
+import { FaGoogle } from "react-icons/fa";
+// import { useLocation, useNavigate } from "react-router-dom";
+// import { AuthContext } from "../../../providers/AuthProvider";
+
+const SocialLogin = () => {
  
+   
+  return (
+    <div>
+      <div className="divider">OR</div>
+      <div className="text-center">
+        <button
+        //   onClick={handleGoogleSignIn}
+          className="btn btn-circle btn-outline"
+        >
+          <FaGoogle />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
